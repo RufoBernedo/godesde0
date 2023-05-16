fdd4335714fad55df55a1324f92e34689f34d416
Rufino Bernedo <rufino.bernedo@gmail.com>
2023-05-15 22:26:14 -0400

    siguiente commit

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"godesde0/variables"
 )
 
 func main() {
 	variables.MuestroEnteros()
+	variables.RestoVariables()
+
+	estado, texto := variables.ConvierteaTexto(1234)
+
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
