958981a3e1bd69c4d0af89149612604b90cacefd
Rufino Bernedo <rufino.bernedo@gmail.com>
2023-05-15 22:38:34 -0400

    if y switch

--- main.go
+++ main.go
@@ -2,15 +2,30 @@ package main
 
 import (
 	"fmt"
-	"godesde0/variables"
+	"runtime"
 )
 
 func main() {
-	variables.MuestroEnteros()
-	variables.RestoVariables()
+	// variables.MuestroEnteros()
+	// variables.RestoVariables()
 
-	estado, texto := variables.ConvierteaTexto(1234)
+	// estado, texto := variables.ConvierteaTexto(1234)
 
-	fmt.Println(estado)
-	fmt.Println(texto)
+	// fmt.Println(estado)
+	// fmt.Println(texto)
+
+	if os := runtime.GOOS; os == "Linux." || os == "OS X." || os == "darwin" {
+		fmt.Println("Esto no es Windows")
+	} else {
+		fmt.Println("Esto es Windows, es ", os)
+	}
+
+	switch os := runtime.GOOS; os {
+	case "darwin1":
+		fmt.Println("Esto es Darwin")
+	case "Windows":
+		fmt.Println("Esto es Windows")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
